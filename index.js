ebe8af9cee7c7149e71fd6b26d1a2897a4defeb3
apusikder118 <apusikder118@gmail.com>
2023-09-21 01:10:48 +0600

    onclick

---
+++ index.js
@@ -0,0 +1,6 @@
+function Clickme(){
+    document.write(" Hi Sir, How Are You ?")
+}
+function Problem(){
+    document.write("I am trying my best. I wrote the code after watching YouTube videos. I couldn't push the code. That problem is solved. Arad helped me. Please keep me in this batch. I will be more active going forward. Ask me to know more about my problem.") 
+}
